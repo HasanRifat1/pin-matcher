cafbe46ac0079a7c2d5744dca9bab897014b3b0e
Najmul Hasan Rifat <rifatr729@gmail.com>
2022-02-14 23:45:23 +0600

    try function done

--- app.js
+++ app.js
@@ -34,16 +34,53 @@ document.querySelector('#parent').addEventListener('click',(event)=>{
 
 })
 //!.....typed value checked......
-document.querySelector('#submit').addEventListener('click',(event)=>{
-    event.preventDefault();
-    const calcValue=document.querySelector('#calc-input').value;
-    // console.log(calcValue);
+// document.querySelector('#submit').addEventListener('click',(event)=>{
+//     event.preventDefault();
+//     const calcValue=document.querySelector('#calc-input').value;
+//     // console.log(calcValue);
+//     const pinValue=document.querySelector('#pinValue').value;
+//     if(calcValue===pinValue){
+//         document.querySelector('#match').style.display='block';
+//         document.querySelector('#notMatch').style.display='none';
+//     }else{
+//         document.querySelector('#match').style.display='none';
+//         document.querySelector('#notMatch').style.display='block';
+//     }
+// })
+//!....try check....
+function tryMoreCheck(){
+    let tryValue=document.querySelector('#tryValue').innerText;
+    // console.log(tryValue);
     const pinValue=document.querySelector('#pinValue').value;
-    if(calcValue===pinValue){
+    const calcValue=document.querySelector('#calc-input').value;
+
+    document.querySelector('#pinGenerate').addEventListener('click',()=>{
+        document.querySelector('#tryValue').innerText=3;
+    })
+
+    
+    if(pinValue!=calcValue){
+        if(tryValue>0){
+            document.querySelector('#tryValue').innerText=parseInt(tryValue)-1;
+            //!....success or fail message......
+            document.querySelector('#match').style.display='none';
+            document.querySelector('#notMatch').style.display='block';
+        }
+    }
+    else if(pinValue==calcValue){
+        document.querySelector('#tryValue').innerText=3;
+        //!....success or fail message......
         document.querySelector('#match').style.display='block';
         document.querySelector('#notMatch').style.display='none';
-    }else{
-        document.querySelector('#match').style.display='none';
-        document.querySelector('#notMatch').style.display='block';
     }
+
+
+    if(tryValue==1){
+        document.querySelector('#submit').setAttribute('disabled',true);
+    }
+}
+
+//!....submit button.....
+document.querySelector('#submit').addEventListener('click',()=>{
+    tryMoreCheck();
 })
